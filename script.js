883fe33232e7b3ac605dfc49c1cb183f8d33da7a
Mohamed Radwan <101032571+mo0hamedRadwan@users.noreply.github.com>
2023-04-24 20:29:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Button 4 Script
+const button = document.querySelector(".btn-4 a");
+
+button.addEventListener("click", function(e) {
+    let x = e.clientX - e.target.offsetLeft;
+    let y = e.clientY - e.target.offsetTop;
+    
+    let ripples = document.createElement("span");
+    ripples.style.left = x + "px";
+    ripples.style.top = y + "px";
+
+    this.appendChild(ripples);
+
+    setTimeout(() => {
+        ripples.remove();
+    }, 1000);
+});
